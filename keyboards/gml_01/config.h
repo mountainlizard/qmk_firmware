be21a95f377b4669b30f122f41ad6b4e9c2f38b7
trepidacious <trepidacious@gmail.com>
2022-09-02 12:01:22 +0100

    Create new keyboard with 1 switch for all keys, OLED enabled

---
+++ keyboards/gml_01/config.h
@@ -0,0 +1,47 @@
+// Copyright 2022 Ben Webster (@trepidacious)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "config_common.h"
+
+/*
+#define MATRIX_COL_PINS \
+    { GP4 }
+#define MATRIX_ROW_PINS \
+    { GP5 }
+*/
+
+#define DEBUG_MATRIX_SCAN_RATE
+
+#define QMK_WAITING_TEST_BUSY_PIN GP8
+#define QMK_WAITING_TEST_YIELD_PIN GP9
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+#define RGB_DI_PIN A1
+
+// OLED on pins GP16 and GP17 (standard Pico labelling)
+#define OLED_DISPLAY_128X32
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP16
+#define I2C1_SCL_PIN GP17
+
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keyboards/gml_01/rules.mk
@@ -0,0 +1,12 @@
+
+# TODO remove, specified in info.json?
+# MCU = RP2040
+# BOOTLOADER = rp2040
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+
+OPT_DEFS += -DHAL_USE_I2C=TRUE
+
+# TODO remove, specified in info.json?
+# CONSOLE_ENABLE = yes
