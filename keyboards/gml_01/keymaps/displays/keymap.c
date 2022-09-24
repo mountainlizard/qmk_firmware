f360eea037d9555c2ca921dd87451dc6dd49ab47
trepidacious <trepidacious@gmail.com>
2022-09-24 14:55:57 +0100

    Add mac brightness controls

--- keyboards/gml_01/keymaps/displays/keymap.c
+++ keyboards/gml_01/keymaps/displays/keymap.c
@@ -72,15 +72,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |Enter |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / | Mac- | Mac+ |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Reset |      |      |      |      |             |      | Next | Dis- | Dis+ | Play |
  * `-----------------------------------------------------------------------------------'
+ *
+ * Mac- = macOS brightness down
+ * Mac+ = macOS brightness up
+ * Dis- = keyboad display brightness down
+ * Dis+ = keyboad display brightness up
  */
 [2] = LAYOUT(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_NUHS, KC_NUBS, KC_SLCK, KC_PAUS, _______,
   _______, _______, _______, _______, _______, _______,  _______, _______, KC_MNXT, KC_DISPLAY_DOWN, KC_DISPLAY_UP, KC_MPLY
 ),
 
@@ -90,16 +95,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  | Left |  Up  | Down |Right | Home | End  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # | Back | Forw |      |Enter |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # | Prev | Next |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Reset |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |Reset |      |      |      |      |             |      | Stop | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [3] = LAYOUT(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    QK_BOOTLOADER,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, KC_HOME, KC_END,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_NUHS, KC_MPRV, KC_MNXT, _______, _______,
-  _______, _______, _______, _______, _______, _______,  _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, _______, _______, _______, _______, _______,  _______, _______, KC_MSTP, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 };
